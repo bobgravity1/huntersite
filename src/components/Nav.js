fa84595cfbe6177c1f821dc43078a229ec11ba94
Robert <Bobgravity1@Roberts-MacBook-Pro-4.local>
2021-02-22 19:57:32 +0000

    asd

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -29,7 +29,7 @@ const toucherOff=()=>{
 /*  NAVBAR */
 <>
 <div className='shadow-xl'>
-    <div  className='nav overflow-hidden w-full nav-main md:pt-8 pt-4  flex  justify-between md:justify-around items-center flex-row flex-shadow-2xl'>
+    <div  className='nav c overflow-hidden w-full nav-main md:pt-8 pt-4  flex  justify-between md:justify-around items-center flex-row flex-shadow-2xl'>
         <h1 className=' mx-2   antialiased  text-lg md:text-3xl font-bold text-green-700 font-second-bold'>HUNTER SOLICITERS</h1>
         <div className={`${active?'nav-items-active':'nav-items'}  bg-white items-center justify-start md:items-center flex flex-col justify-center md:flex-row md:justify-around list-none w-1/3   flex-shadow-2xl`}>
         <Link onClick={()=>setActive(false)} to='/about' className='text-green-800 hover:text-green-800 font-semibold duration-200 antialiased  p-2 cursor-pointer lg:my-0 my-6 text-sm lg:hover:text-white lg:hover:bg-red-900 font-main'>About</Link>
